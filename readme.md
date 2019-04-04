bed5f76de80c892f8de2ffd7c60ef3c342db8a1b
Daniel Herrera <dan_michigan2012@hotmail.com>
2019-04-04 15:32:56 -0600

    API update
    Changed api, as well as other asthetic changes.

--- readme.md
+++ readme.md
@@ -1,17 +1,20 @@
 # Weather Website
-> A small website that takes a user entered zipcode and retrieves the current weather using the OpenWeatherMap API.
+> A small website that takes a user entered zipcode and retrieves the current weather using the AccuWeather API
 
 CSS icons were made by Tiffany Du and were retrieved from https://codepen.io/tiffanyadu/pen/OprNrV.
 Text animations were made by Brielle and were retrieved from https://codepen.io/buntinb/pen/OEjENG.
 
 Sample screenshot
+![](screenshot.png)
 
-![](screenshot.PNG)
+# Testing
+
+The site was tested on both mobile and desktop using both Chrome and Firefox. Zipcodes entered that aren't 5 digits long require the user to enter another zipcode. Zipcodes from outside of the US are also not considered valid. Zipcodes that contain letters or other characters are not valid. 
 
 # Issues
-The OpenWeatherMap API doesn't have the most accurate zipcode data. For example, when given the zipcode of my hometown Burlington CO (80807), it gives the location as Colorado Springs.
 
 The CSS icons don't seem to play well with the animations. When new icons are drawn there is a shift in the icons. For example, when the rain icon loads the rain lines will appear vertical at first and then shift to an angle when the animations are finished. 
 
 # Possible Improvements
-Possible improvements would include fixing the issues stated above, as well as adding an extended forecast.
+
+Possible improvements would include fixing the issue stated above, as well as adding an extended forecast.
--- scripts.js
+++ scripts.js
@@ -5,57 +5,80 @@ function zipCheck(zip) {
         return true
     }
     else {
-        alert("Please enter a valid zipcode.")  //Alert user if zipcode is invalid
-        return false
+        clearElements()
+        drawInvalidZip()
+        return false   //Return False if invalid zipcode
     }
 }
-/*Function to retreive api data*/
-function getWeather() {
-    clearElements()
+function getLocationKey() {
     var zip = document.getElementById("zipcode").value  //Retrieve zipcode from user
+    var location;
+    var locationkey;
+    var usZip = false;
     if (zipCheck(zip)) {
-        fetch("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=cb84c09f90fda0da9231d0989f6977bd&units=imperial")  //Request data from API
+        fetch("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=1zGOaKyKxS4Os4itr9J8w0rZddeOK3XM&q=" + zip + "&language=en-us&details=true")  //Request data from API
             .then(function (data) { return data.json(); })  //Retrieve json object
             .then(function (myJson) {
-                drawLocation(myJson)  //Draw elements in order
-                drawIcon(myJson)
-                drawText(myJson)
+                myJson.forEach(element => {
+                    if (element.Country.ID == "US") {
+                        location = element.LocalizedName
+                        locationkey = element.Key
+                        getWeather(locationkey)
+                        drawLocation(location)
+                        usZip = true
+                    }
+                });
+                if (usZip == false) {
+                    clearElements()
+                    drawZipNotFound()
+                }
             })
-
     }
+    console.log(usZip)
+
+}
+/*Function to retreive api data*/
+function getWeather(locationkey) {
+    clearElements()
+    fetch("http://dataservice.accuweather.com/currentconditions/v1/" + locationkey + "?apikey=1zGOaKyKxS4Os4itr9J8w0rZddeOK3XM&language=en-us&details=true")  //Request data from API
+        .then(function (data) { return data.json(); })  //Retrieve json object
+        .then(function (myJson) {
+            drawIcon(myJson[0].WeatherText)    //Draw elements in order
+            drawText(myJson)
+        })
 }
 /*Function to generate location heading*/
-function drawLocation(myJson){
+function drawLocation(locationName) {
     var element = document.getElementById("weather")  //Generate elements
     var location = document.createElement("h2")
-    location.appendChild(document.createTextNode("Location: " + myJson.name))  //Append elements with content
+    location.appendChild(document.createTextNode("Location: " + locationName))  //Append elements with content
     element.appendChild(location)  //Append elements to div element
 }
 /*Function to generate weather text*/
 function drawText(myJson) {
     var element = document.getElementById("weather")  //Generate elements
-    var temperature = Math.round(Number(myJson.main.temp))  //Get temp and round it
+    var temperature = Math.round(Number(myJson[0].Temperature.Imperial.Value))  //Get temp and round it
     var temp = document.createElement("h2")
     var humidity = document.createElement("h2")
     var description = document.createElement("h2")
     var textDiv = document.createElement("div")
-    textDiv.className = "weatherText"  
+    textDiv.className = "weatherText"
     element.appendChild(textDiv)   //Append elements with content
     temp.appendChild(document.createTextNode("Temp: " + temperature + " \xB0" + "F"))
-    humidity.appendChild(document.createTextNode("Humidity: " + myJson.main.humidity + "%"))
-    description.appendChild(document.createTextNode(myJson.weather[0].description.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')))
+    humidity.appendChild(document.createTextNode("Humidity: " + myJson[0].RelativeHumidity + "%"))
+    description.appendChild(document.createTextNode(myJson[0].WeatherText.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')))
     textDiv.appendChild(description)  //Append elements to div element
     textDiv.appendChild(temp)
     textDiv.appendChild(humidity)
 }
 /*Function to generate weather icon*/
-function drawIcon(myJson) {
+function drawIcon(weatherText) {
     var element = document.getElementById("weather")  //Generate elements
     var weatherIcon = document.createElement("div")
     var weatherDesc = document.createElement("div")
     var weatherClass = document.createElement("div")
     weatherIcon.className = "weatherIcon"
-    weatherDesc.className = getIcon(myJson)  //Retrieve weather desription from json object
+    weatherDesc.className = weatherText  //Retrieve weather desription from json object
     weatherClass.className = "inner"
     element.appendChild(weatherIcon)  //Append elements to div element
     weatherIcon.appendChild(weatherDesc)
@@ -70,41 +93,49 @@ function clearElements() {
         document.getElementById("weather").innerHTML = ""
     }
 }
+function drawInvalidZip() {
+    var element = document.getElementById("weather")  //Generate elements
+    var message = document.createElement("h2")
+    message.appendChild(document.createTextNode("Please enter a 5 digit zipcode."))
+    element.appendChild(message)
+
+}
+function drawZipNotFound() {
+    var element = document.getElementById("weather")  //Generate elements
+    var message = document.createElement("h2")
+    message.appendChild(document.createTextNode("Location not found. Please try again."))
+    element.appendChild(message)
+}
 /*Function to retreive correct weather icon*/
-function getIcon(myJson){
-    if (myJson.weather[0].description == "few clouds" || myJson.weather[0].description == "scattered clouds"){
+function getIcon(myJson) {
+    if (myJson[0].WeatherText.toLowerCase in ["partly sunny", "intermittent clouds"]) {
         return "partlycloudy"
     }
-    else if (myJson.weather[0].description == "broken clouds"){
+    else if (myJson[0].WeatherText.toLowerCase == "mostly cloudy") {
         return "mostlycloudy"
     }
-    else if(myJson.weather[0].description == "overcast clouds"){
+    else if (myJson[0].WeatherText.toLowerCase in ["cloudy", "dreary"]) {
         return "cloudy"
     }
-    else if(myJson.weather[0].description == "clear sky"){
+    else if (myJson[0].WeatherText.toLowerCase in ["sunny", "mostly Sunny", "clear", "mostly clear"]) {
         return "clear"
     }
-    else if(myJson.weather[0].description == "thunderstorm with light rain" || myJson.weather[0].description == "thunderstorm with rain" 
-    || myJson.weather[0].description == "thunderstorm with heavy rain" || myJson.weather[0].description == "light thunderstorm"
-    || myJson.weather[0].description == "thunderstorm" || myJson.weather[0].description == "heavy thunderstorm" || myJson.weather[0].description == "ragged thunderstorm" 
-    || myJson.weather[0].description == "thunderstorm with light drizzle" || myJson.weather[0].description == "thunderstorm with drizzle" || myJson.weather[0].description == "thunderstorm with heavy drizzle"){
+    else if (myJson[0].WeatherText.toLowerCase in ["T-Storms", "mostly cloudy w/ t-storms", "partly sunny w/ t-storms"]) {
         return "tstorms"
     }
-    else if(myJson.weather[0].description == "light snow" || myJson.weather[0].description == "snow" || myJson.weather[0].description == "heavy snow"){
+    else if (myJson[0].WeatherText in ["snow", "mostly cloudy w/snow"]) {
         return "snow"
     }
-    else if(myJson.weather[0].description == "sleet" || myJson.weather[0].description == "light shower sleet" || myJson.weather[0].description == "shower sleet"
-    || myJson.weather[0].description == "light rain and snow" || myJson.weather[0].description == "rain and snow" || myJson.weather[0].description == "light shower snow"
-    || myJson.weather[0].description == "shower snow" || myJson.weather[0].description == "heavy shower snow"){
+    else if (myJson[0].WeatherText in ["sleet", "freezing rain", "ice", "rain and snow"]) {
         return "sleet"
     }
-    else if(myJson.weather[0].description == "mist" || myJson.weather[0].description == "smoke" || myJson.weather[0].description == "haze"
-    || myJson.weather[0].description == "sand/dust whirls" || myJson.weather[0].description == "fog" || myJson.weather[0].description == "sand"
-    || myJson.weather[0].description == "dust" || myJson.weather[0].description == "volcanic ash" || myJson.weather[0].description == "squalls"
-    || myJson.weather[0].description == "tornado"){
+    else if (myJson[0].WeatherText in ["flurries", "mostly cloudy w/ flurries", "partly sunny w/ flurries"]) {
+        return "flurries"
+    }
+    else if (myJson[0].WeatherText in ["fog", "hazy sunshine"]) {
         return "fog"
     }
-    else{
+    else if (myJson[0].WeatherText in ["showers", "mostly cloudy w/ showers", "partly sunny w/ showers", "rain"]) {
         return "rain"
     }
 }
